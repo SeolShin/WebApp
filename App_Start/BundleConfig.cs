3cf44100a330d61ed572addbd7779f49357e4eec
SeolShin <39841236+SeolShin@users.noreply.github.com>
2018-06-18 23:44:08 +0200

    Add files via upload

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,55 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApp
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Assets/css").Include(
+                "~/assets/vendor/bootstrap/css/bootstrap.css",
+                "~/assets/vendor/fontawesome/css/font-awesome.min.css",
+                "~/assets/vendor/flaticons/flaticon.css",
+                "~/assets/vendor/hover/css/hover-min.css",
+                "~/assets/vendor/wow/animate.css",
+                "~/assets/vendor/mfp/css/magnific-popup.css",
+                "~/assets/vendor/footable/footable.core.css",
+                "~/assets/custom/css/style.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/Assets/js").Include(
+                "~/assets/vendor/jquery/js/jquery-2.2.0.min.js",
+                "~/assets/vendor/bootstrap/js/bootstrap.min.js",
+                "~/assets/vendor/imagesloaded/js/imagesloaded.pkgd.min.js",
+                "~/assets/vendor/mfp/js/jquery.magnific-popup.min.js",
+                "~/assets/vendor/circle-progress/circle-progress.js",
+                "~/assets/vendor/wow/wow.min.js",
+                "~/assets/vendor/pjax/jquery.pjax.js",
+                "~/assets/vendor/footable/footable.all.min.js",
+                "~/assets/custom/js/custom.js"
+                ));
+
+        }
+    }
+}
---
+++ Controllers/StoreController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class StoreController : Controller
+    {
+        KETIEntities storeDB = new KETIEntities();
+        //private AlbumDBContext storeDB = new AlbumDBContext();
+
+        /*
+        // GET: Store
+        public ActionResult Index(string subject)
+        {
+            var item = storeDB.single_photo.ToList();
+            
+            //var o = new single_photo { objectName = subject };
+            //return View(o);            
+            return View(item);
+        }
+        */
+
+        public ActionResult Index()
+        {
+            //var item = storeDB.object_list.ToList();   
+            var item = storeDB.single_photo.ToList();
+            /*             
+            var item = new List<subject_list>
+            {
+                new subject_list { Name = "freetime" },
+                new subject_list { Name = "gear" }
+             };
+             */
+            return View(item);
+        }
+        
+        public ActionResult Display()
+        {
+            var item = storeDB.single_photo.ToList();
+            return View(item);
+        }
+
+        /*
+        public byte[] GetImageFromDataBase(int Id = 1)
+        {
+            var single_data = storeDB.single_photo.Find(Id);
+            //byte[] canvas = single_data.img.First();
+            //return canvas;
+        }
+        */
+
+    }
+
+
+}
